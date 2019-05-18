900f516cb909474f3d6a17746533cefa04186542
tommenx <tommenx09@outlook.com>
2019-05-18 15:30:50 +0800

    modify build script

--- build/build-lvm.sh
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env bash
-# 非0返回值，立即退出脚本执行
-set -e
-
-cd ${GOPATH}/src/github.com/tommenx/csi-lvm-plugin/cmd
-
-# 测试代码，编辑本机版本
-go build -o lvmplugin.csi.alibabacloud.com
-mv lvmplugin.csi.alibabacloud.com ../build/lvm
